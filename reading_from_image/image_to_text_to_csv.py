4dbfc642b923fb8364763375c552cf19d357cb75
cnr-nar <66534873+cnr-nar@users.noreply.github.com>
2023-03-10 12:37:08 +0300

    Update image_to_text_to_csv.py

--- reading_from_image/image_to_text_to_csv.py
+++ reading_from_image/image_to_text_to_csv.py
@@ -4,19 +4,20 @@ import easyocr
 from array import *
 import csv
 
-reader = easyocr.Reader(['en'])
+reader = easyocr.Reader(['en']) # Image to text trained frame
 from pathlib import Path  
 
 list_el = []
-
-for i in range(0,236):
+# Gets Images 
+for i in range(0,236): #This just manually checked image number
     path = "images\image_"+str(i)+".jpg"
     result = reader.readtext(path, paragraph= False)
     df = pd.DataFrame(result)
     list_el.append(df[1])
     print("image"+str(i)+"done")
 
-df_out = pd.DataFrame(list_el)
+# creates a dataFrame to write datas in a csv file
+df_out = pd.DataFrame(list_el) 
 filepath = Path('to_csv/read_images.csv')  
 filepath.parent.mkdir(parents=True, exist_ok=True)  
 df_out.to_csv(filepath)  
